4f60583ee59c4defda6a3b9383530cc07a5ac63b
Owen Travis <otravis@princeton.edu>
2022-03-14 14:26:43 -0400

    new sql file

--- tigertravel.sql
+++ tigertravel.sql
@@ -1,36 +1,103 @@
-DROP TABLE IF EXISTS students;
+--
+-- PostgreSQL database dump
+--
 
-CREATE TABLE students
-(netid TEXT, firstname TEXT, lastname TEXT, email TEXT, phone TEXT, strikes INTEGER);
+-- Dumped from database version 14.2
+-- Dumped by pg_dump version 14.2
 
-INSERT INTO students (netid, firstname, lastname, email, phone, strikes)
-   VALUES ('sydneyp','Sydney', 'Pittignano', 'sydneyp@princeton.edu', '(203)-914-7848', 0);
-INSERT INTO students (netid, firstname, lastname, email, phone, strikes)
-   VALUES ('manyaz','Manya', 'Zhu', 'manyaz@princeton.edu', '(609)-456-2795', 0);
-INSERT INTO students (netid, firstname, lastname, email, phone, strikes)
-   VALUES ('otravis','Owen', 'Travis', 'otravis@princeton.edu', '(847)-691-8322', 1);
-INSERT INTO students (netid, firstname, lastname, email, phone, strikes)
-   VALUES ('bbob','Billy', 'Bob', 'bbob@bob.com', '(111)-111-1111', 1);
-INSERT INTO students (netid, firstname, lastname, email, phone, strikes)
-   VALUES ('jbob','Joey', 'Bob', 'jbob@bob.com', '(222)-222-2222', 2);
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
--- ---------------------------------------------------------------------
+SET default_tablespace = '';
 
-DROP TABLE IF EXISTS rides;
+SET default_table_access_method = heap;
 
-CREATE TABLE rides
-(rideid INTEGER, startdate TEXT, enddate TEXT, origin TEXT, dest TEXT, starttime INTEGER, endtime INTEGER, num INTEGER);
+--
+-- Name: riders; Type: TABLE; Schema: public; Owner: ttadmins
+--
 
-INSERT INTO rides (rideid, startdate, enddate, origin, dest, starttime, endtime, num)
-   VALUES (1, '2-6-2022', '2-6-2022', 'Princeton', 'JFK', 100, 300, 1);
+CREATE TABLE public.riders (
+    rideid integer,
+    netid text
+);
 
--- ---------------------------------------------------------------------
 
-DROP TABLE IF EXISTS riders;
+ALTER TABLE public.riders OWNER TO ttadmins;
 
-CREATE TABLE orders
-(rideid INTEGER, netid TEXT);
+--
+-- Name: rides; Type: TABLE; Schema: public; Owner: ttadmins
+--
 
-INSERT INTO rider (rideid, netid)
-   VALUES (1,'sydneyp');
+CREATE TABLE public.rides (
+    rideid integer,
+    startdate text,
+    enddate text,
+    origin text,
+    dest text,
+    starttime integer,
+    endtime integer,
+    num integer
+);
+
+
+ALTER TABLE public.rides OWNER TO ttadmins;
+
+--
+-- Name: students; Type: TABLE; Schema: public; Owner: ttadmins
+--
+
+CREATE TABLE public.students (
+    netid text,
+    firstname text,
+    lastname text,
+    email text,
+    phone text,
+    strikes integer
+);
+
+
+ALTER TABLE public.students OWNER TO ttadmins;
+
+--
+-- Data for Name: riders; Type: TABLE DATA; Schema: public; Owner: ttadmins
+--
+
+COPY public.riders (rideid, netid) FROM stdin;
+1	sydneyp
+\.
+
+
+--
+-- Data for Name: rides; Type: TABLE DATA; Schema: public; Owner: ttadmins
+--
+
+COPY public.rides (rideid, startdate, enddate, origin, dest, starttime, endtime, num) FROM stdin;
+1	2-6-2022	2-6-2022	Princeton	JFK	100	300	1
+\.
+
+
+--
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: ttadmins
+--
+
+COPY public.students (netid, firstname, lastname, email, phone, strikes) FROM stdin;
+sydneyp	Sydney	Pittignano	sydneyp@princeton.edu	(203)-914-7848	0
+manyaz	Manya	Zhu	manyaz@princeton.edu	(609)-456-2795	0
+otravis	Owen	Travis	otravis@princeton.edu	(847)-691-8322	1
+bbob	Billy	Bob	bbob@bob.com	(111)-111-1111	1
+jbob	Joey	Bob	jbob@bob.com	(222)-222-2222	2
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
 
