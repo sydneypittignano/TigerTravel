20067797f6ba0a544ecb153a803ac036fd1a034d
Owen Travis <otravis@princeton.edu>
2022-03-14 00:29:44 -0400

    basic database prototypes

---
+++ display.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# display.py
+# Authors: Owen Travis, Manya Zhu
+#-----------------------------------------------------------------------
+
+from sys import argv, stderr, exit
+from psycopg2 import connect
+
+#-----------------------------------------------------------------------
+
+def main():
+
+    if len(argv) != 1:
+        print('Usage: python display.py', file=stderr)
+        exit(1)
+
+    try:
+        with connect(
+            host='localhost', port=5432, user='otravis', password='otravis62737696',
+            database='tigertravel') as connection:
+
+            with connection.cursor() as cursor:
+
+                print('-------------------------------------------')
+                print('students')
+                print('-------------------------------------------')
+                cursor.execute("SELECT * FROM students")
+                row = cursor.fetchone()
+                while row is not None:
+                    print(row)
+                    row = cursor.fetchone()
+
+                print('-------------------------------------------')
+                print('rides')
+                print('-------------------------------------------')
+                cursor.execute("SELECT * FROM rides")
+                row = cursor.fetchone()
+                while row is not None:
+                    print(row)
+                    row = cursor.fetchone()
+
+                print('-------------------------------------------')
+                print('riders')
+                print('-------------------------------------------')
+                cursor.execute("SELECT * FROM riders")
+                row = cursor.fetchone()
+                while row is not None:
+                    print(row)
+                    row = cursor.fetchone()
+
+    except Exception as ex:
+        print(ex, file=stderr)
+        exit(1)
+
+#-----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    main()
---
+++ tigertravel.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students
+(netid TEXT, firstname TEXT, lastname TEXT, email TEXT, phone TEXT, strikes INTEGER);
+
+INSERT INTO students (netid, firstname, lastname, email, phone, strikes)
+   VALUES ('sydneyp','Sydney', 'Pittignano', 'sydneyp@princeton.edu', '(203)-914-7848', 0);
+INSERT INTO students (netid, firstname, lastname, email, phone, strikes)
+   VALUES ('manyaz','Manya', 'Zhu', 'manyaz@princeton.edu', '(609)-456-2795', 0);
+INSERT INTO students (netid, firstname, lastname, email, phone, strikes)
+   VALUES ('otravis','Owen', 'Travis', 'otravis@princeton.edu', '(847)-691-8322', 1);
+
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS rides;
+
+CREATE TABLE rides
+(rideid INTEGER, startdate TEXT, enddate TEXT, origin TEXT, dest TEXT, starttime INTEGER, endtime INTEGER, num INTEGER);
+
+INSERT INTO rides (rideid, startdate, enddate, origin, dest, starttime, endtime, num)
+   VALUES (1, '2-6-2022', '2-6-2022', 'Princeton', 'JFK', 100, 300, 1);
+
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS riders;
+
+CREATE TABLE orders
+(rideid INTEGER, netid TEXT);
+
+INSERT INTO rider (rideid, netid)
+   VALUES (1,'sydneyp');
+
