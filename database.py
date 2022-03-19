4b665d138579854df0a910256c322818b98923d7
Manya Zhu <manyaz@princeton.edu>
2022-03-19 15:18:24 -0400

    database stuff

--- database.py
+++ database.py
@@ -6,6 +6,7 @@
 #-----------------------------------------------------------------------
 
 from psycopg2 import connect
+from ride import Ride
 
 #-----------------------------------------------------------------------
 
@@ -68,6 +69,137 @@ def filter_rides(startdate, enddate, origin, dest, starttime, endtime):
     return rides
 
 #-----------------------------------------------------------------------
+ 
+def add_student(netid, firstname, lastname, email, phone, strikes):
+   with connect(
+       host='localhost', port=5432, user='ttadmins',
+       password='071020010307200204262002oms', database='tigertravel') as connection:
+ 
+       with connection.cursor() as cursor:
+ 
+           info = [netid, firstname, lastname, email, phone, strikes]
+           query_str = "INSERT INTO students (netid, firstname, lastname, "
+           query_str += "email, phone, strikes) VALUES ?;"
+ 
+           cursor.execute(query_str, info)
+ 
+   return
+  
+#-----------------------------------------------------------------------
+ 
+def add_strike(netid):
+   with connect(
+       host='localhost', port=5432, user='ttadmins',
+       password='071020010307200204262002oms', database='tigertravel') as connection:
+ 
+       with connection.cursor() as cursor:
+ 
+           query_str = "UPDATE students SET strikes=strikes+1 WHERE netid=?;"
+ 
+           cursor.execute(query_str, [netid])
+ 
+   return
+ 
+#-----------------------------------------------------------------------
+ 
+def add_ride(netid, rideid, startdate, enddate, origin, dest, starttime, endtime, num):
+   with connect(
+       host='localhost', port=5432, user='ttadmins',
+       password='071020010307200204262002oms', database='tigertravel') as connection:
+ 
+       with connection.cursor() as cursor:
+ 
+           info = [rideid, startdate, enddate, origin, dest, starttime, endtime, num]
+           query_str = "INSERT INTO rides (rideid, startdate, enddate, origin, dest, "
+           query_str += "starttime, endtime, num) VALUES ?;"
+ 
+           cursor.execute(query_str, info)
+ 
+           query_str = "INSERT INTO riders (rideid, netid) VALUES ?;"
+          
+           cursor.execute(query_str, [rideid, netid])
+ 
+   return
+ 
+#-----------------------------------------------------------------------
+ 
+def join_ride(netid, prev_rideid, new_rideid):
+   with connect(
+       host='localhost', port=5432, user='ttadmins',
+       password='071020010307200204262002oms', database='tigertravel') as connection:
+ 
+       with connection.cursor() as cursor:
+ 
+           query_str = "UPDATE riders SET rideid = ? WHERE "
+           query_str += "netid = ? AND rideid = ?;"
+ 
+           cursor.execute(query_str, [prev_rideid, netid, new_rideid])
+ 
+           #should the old ride in rides stay the same? should we change rideid?
+ 
+   return
+ 
+#-----------------------------------------------------------------------
+ 
+def filter_rides(startdate, enddate, origin, dest, starttime, endtime):
+   list = []
+   if startdate is not None:
+       list.append(startdate)
+   if enddate is not None:
+       list.append(enddate)
+   if origin is not None:
+       list.append(origin)
+   if dest is not None:
+       list.append(dest)
+   if starttime is not None:
+       list.append(starttime)
+   if endtime is not None:
+       list.append(endtime)
+ 
+   with connect(
+       host='localhost', port=5432, user='ttadmins',
+       password='071020010307200204262002oms', database='tigertravel') as connection:
+ 
+       with connection.cursor() as cursor:
+ 
+           stmt_str = "SELECT riders.netid, rides.startdate, rides.enddate, rides.origin, "
+           stmt_str += "rides.dest, rides.starttime, rides.endtime FROM rides, riders "
+           stmt_str += "WHERE rides.rideid=riders.rideid "
+ 
+           if startdate is not None:
+               stmt_str += "AND rides.startdate LIKE ? "
+           if enddate is not None:
+               stmt_str += "AND rides.enddate "
+               stmt_str += "LIKE ? "
+           if origin is not None:
+               stmt_str += "AND rides.origin LIKE ? "
+           if dest is not None:
+               stmt_str += "AND rides.dest LIKE ? "
+           if starttime is not None:
+               stmt_str += "AND rides.starttime LIKE ? "
+           if endtime is not None:
+               stmt_str += "AND rides.endtime LIKE ? "
+ 
+           stmt_str += "ORDER BY rides.startdate ASC, "
+           stmt_str += "rides.starttime ASC, "
+           stmt_str += "rides.origin ASC;"
+ 
+           cursor.execute(stmt_str, list)
+           row = cursor.fetchone()
+ 
+           table = []
+ 
+           while row is not None:
+               ride_row = Ride(str(row[0]), str(row[1]),
+               str(row[2]), str(row[3]), str(row[4]), str(row[5]), str(row[6]),
+               str(row[7]))
+               table.append(ride_row)
+               row = cursor.fetchone()
+ 
+           return table
+ 
+#-----------------------------------------------------------------------
+
 
 # For testing:
 
---
+++ ride.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+ 
+#-----------------------------------------------------------------------
+# ride.py
+# Author: Manya Zhu
+#-----------------------------------------------------------------------
+ 
+class Ride:
+ 
+   def __init__(rideid, startdate, enddate, origin, dest, starttime, endtime, num):
+       self._rideid = rideid
+       self._startdate = startdate
+       self._enddate = enddate
+       self._origin = origin
+       self._dest = dest
+       self._starttime = starttime
+       self._endtime = endtime
+       self._num = num
+ 
+   def get_rideid(self):
+       return self._rideid
+ 
+   def get_startdate(self):
+       return self._startdate
+ 
+   def get_enddate(self):
+       return self._enddate
+ 
+   def get_origin(self):
+       return self._origin
+ 
+   def get_dest(self):
+       return self._dest
+ 
+   def get_starttime(self):
+       return self._starttime
+  
+   def get_endtime(self):
+       return self._endtime
+  
+   def get_num(self):
+       return self._num
+ 
+   def to_tuple(self):
+       return (self._rideid, self._startdate, self._enddate, self._origin,
+       self._dest, self._starttime, self._endtime, self._num)
+ 
+   def to_dict(self):
+       return {'rideid': self._rideid, 'startdate': self._startdate, 'enddate': self._enddate,
+       'origin': self._origin, 'dest': self._dest, 'starttime': self._starttime,
+       'endtime': self._endtime, 'num': self._num}
+ 
+#-----------------------------------------------------------------------
+ 
+def _test():
+   ride = Ride('1', '2/4/2003', '2/4/2003', 'princeton', 'JFK', 100, 400, 1)
+   print(ride.to_tuple())
+   print()
+   print(ride.to_dict())
+ 
+if __name__ == '__main__':
+   _test()
+ 
