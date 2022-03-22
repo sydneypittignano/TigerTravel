1425196fa090d68538623a28cde8837dc93c14c3
Owen Travis <otravis@princeton.edu>
2022-03-22 00:00:16 -0400

    deleted databse

--- database.py
+++ database.py
@@ -29,45 +29,6 @@ def search_students(netid):
 
     return student
 
-#-----------------------------------------------------------------------
-
-def filter_rides(startdate, enddate, origin, dest, starttime, endtime):
-
-    rides = []
-    #list = []
-    #i = 0
-    #if startdate is not None:
-        #list.append(startdate)
-    #if enddate is not None:
-        #list.append(enddate)
-    
-    with connect(
-        host='localhost', port=5432, user='ttadmins', 
-        password='071020010307200204262002oms', database='tigertravel') as connection:
-
-        with connection.cursor() as cursor:
-
-            query_str = "SELECT * FROM rides"
-            
-            #if startdate is not None:
-                #query_str += " WHERE startdate = %s"
-                #i = i + 1
-            #if enddate is not None:
-                #if (i != 0):
-                    #query_str += " AND enddate = %s"
-                #else:
-                    #query_str += " WHERE enddate = %s"
-            
-            #print(list)
-            #cursor.execute(query_str, list)
-            cursor.execute(query_str)
-
-            row = cursor.fetchone()
-            while row is not None:
-                rides.append(row)
-                row = cursor.fetchone()
-
-    return rides
 
 #-----------------------------------------------------------------------
  
@@ -94,7 +55,7 @@ def add_strike(netid):
        password='071020010307200204262002oms', database='tigertravel') as connection:
  
        with connection.cursor() as cursor:
- 
+
            query_str = "UPDATE students SET strikes=strikes+1 WHERE netid=?;"
  
            cursor.execute(query_str, [netid])
