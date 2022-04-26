1e21d74c94f5330d561f9d7dbe465a98f636b2b6
Owen Travis <otravis@princeton.edu>
2022-04-25 22:22:33 -0400

    altered students table

--- database.py
+++ database.py
@@ -129,8 +129,8 @@ def check_student(netid):
 
 def add_student(cursor, netid):
     info = [netid]
-    stmt_str = "INSERT INTO students (netid, firstname, lastname, "
-    stmt_str += "email, phone, strikes, count) VALUES (%s, '', '', '', '', 0, 1);"
+    stmt_str = "INSERT INTO students (netid, "
+    stmt_str += "strikes, count) VALUES (%s, 0, 1);"
     cursor.execute(stmt_str, info)
 
 #-----------------------------------------------------------------------
