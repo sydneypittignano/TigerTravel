e92e9fc6d96d09de01706a53ad19fdf21cad8d9a
Owen Travis <otravis@princeton.edu>
2022-04-29 04:15:46 -0400

    new browse results

--- tigertravel.sql
+++ tigertravel.sql
@@ -5,6 +5,10 @@
 -- Dumped from database version 14.2
 -- Dumped by pg_dump version 14.2
 
+DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS riders;
+DROP TABLE IF EXISTS rides;
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
